24ad40583c376d422579a1bd3c4b31624843e0ec
Abdelrahman84 <abdelrahman.farag84@gmail.com>
2023-08-26 09:31:46 +0300

    update LoginPage

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,15 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import { Card, Container } from "@chakra-ui/react";
 
-import LoginForm from "./LoginForm";
 import styles from './Login.module.scss';
+import LoginForm from "./LoginForm";
 import routes from "../../../routes";
+import { Values } from "./LoginForm/LoginForm";
+import { AuthApi } from "../../../api/authApi";
+import SwitchPage from "../SwitchPage";
 
 
 function Login() {
     const navigate = useNavigate();
 
-    const handleLogin = (): void => {
-        // TBD open login modal
+    const handleLogin = async (values: Values): Promise<void> => {
+        await AuthApi.checkEmailBeforeLogin(values.email).then(response => {
+            if (response.data.status === 1) {
+                navigate({
+                    pathname: routes.auth.register,
+                    search: `?email=${values.email}`,
+                });
+            }
+        })
     }
 
     const handleGuest = (): void => {
@@ -17,9 +28,13 @@ function Login() {
     }
 
     return (
-        <div className={styles.LoginPage}>
-            <LoginForm onHandleLogin={handleLogin} onHandleGuest={handleGuest}/>
-        </div>
+        <Container className={styles.login}>
+            <Card>
+                <LoginForm onHandleLogin={handleLogin} onHandleGuest={handleGuest} />
+            </Card>
+
+            <SwitchPage className={styles.switchPage} title="Don't have an account?" linkTitle="Sign up" linkUrl={routes.auth.register} />
+        </Container>
     )
 }
 
